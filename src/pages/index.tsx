38249fd9659e7a1bd578e38b4eeec3d255f6b260
Kershal <ericlisfds@hotmail.com>
2022-07-15 22:18:04 -0300

    tabela visual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana' , 34, '1'),
+    new Cliente('Ana' , 34, '1'),
+    new Cliente('Ana' , 34, '1'),
+    new Cliente('Ana' , 34, '1'),
+
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir ${cliente.nome}`)
+  }
+
   return (
     <div className={`
     flex justify-center items-center h-screen bg-cyan-800 text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}
+        clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}
+        ></Tabela>
       </Layout>
     </div>
   )

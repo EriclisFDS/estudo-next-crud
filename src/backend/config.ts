e4f27564d2657eef68b49b707e1a6d87a91216f5
Kershal <ericlisfds@hotmail.com>
2022-07-27 18:38:46 -0300

    crud funcionando sem hooks personalizados

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -12,10 +12,10 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,  
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDO_MAIN,
-    preojectId: process.env.NEXT_PUBLIC_FIREBASE_PRJECT_ID
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
-const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
+const app = !getApps().length ? initializeApp( firebaseConfig ) : getApp()
 const dataBase = getFirestore(app)
 
 export { dataBase }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCLiente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
 
-  const clientes = [
-    new Cliente('Ana' , 34, '1'),
-    new Cliente('Pedro' , 24, '2'),
-    new Cliente('Paula' , 27, '3'),
-    new Cliente('Marcos' , 44, '4'),
+  const repo: ClienteRepositorio = new ColecaoCliente()
 
-  ]
+  const [cliente , setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes , setClientes] = useState<Cliente[]>([])
+  const [visivel, setVisivel] = useState< 'tabela' | 'form' >('tabela')
+
+  useEffect(obterTodos, [])
+
+  // const clientes = [
+  //   new Cliente('Ana' , 34, '1'),
+  //   new Cliente('Pedro' , 24, '2'),
+  //   new Cliente('Paula' , 27, '3'),
+  //   new Cliente('Marcos' , 44, '4'),
+
+  // ]
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente) {
     setCliente(cliente)
     setVisivel("form")
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    console.log(`Excluir ${cliente.nome}`)
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
-  function salvarCliente (cliente: Cliente){
-    console.log(cliente)
-    setVisivel('tabela')
+  async function salvarCliente (cliente: Cliente){
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
   function novoCliente (){
     setCliente(Cliente.vazio())
     setVisivel('form')
   }
-
-  const [cliente , setCliente] = useState<Cliente>(Cliente.vazio())
-  const [visivel, setVisivel] = useState< 'tabela' | 'form' >('tabela')
 
   return (
     <div className={`

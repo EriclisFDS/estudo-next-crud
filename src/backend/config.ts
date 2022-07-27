1bde9268268be4ae7a83ca1e294a18a3a00fdfbb
Kershal <ericlisfds@hotmail.com>
2022-07-27 17:34:41 -0300

    versao nova do firebase

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,20 +1,21 @@
-import firebase from "firebase/app";
-import 'firebase/firestore';
+import { initializeApp, getApps, getApp }from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 
-if(!firebase.getApps.length){
-    firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDO_MAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PRJECT_ID,
-    })
-}
+// if(!firebase.getApps.length){
+//     firebase.initializeApp({
+//         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDO_MAIN,
+//         projectId: process.env.NEXT_PUBLIC_FIREBASE_PRJECT_ID,
+//     })
+// }
 
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,  
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDO_MAIN,
-//     preojectId: process.env.NEXT_PUBLIC_FIREBASE_PRJECT_ID
-// };
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,  
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDO_MAIN,
+    preojectId: process.env.NEXT_PUBLIC_FIREBASE_PRJECT_ID
+};
 
-// const firebase = initializeApp(firebaseConfig);
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
+const dataBase = getFirestore(app)
 
-export default firebase
+export { dataBase }
